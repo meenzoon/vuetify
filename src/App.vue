0d33fc2465a40fdd1162014e2a5c52ef95415dbc
minjun <rlaalswns523@gmail.com>
2020-04-18 01:54:15 +0900

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Header />
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <Footer />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Header,
+    Footer
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-footer
+    app
+    color="primary"
+    dark
+  >
+    <v-card
+      flat
+      tile
+      class="primary white--text text-center mx-auto"
+    >
+      <v-card-text class="white--text">
+        Copyright <strong>meenzoon</strong>. All rights reserved.
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
